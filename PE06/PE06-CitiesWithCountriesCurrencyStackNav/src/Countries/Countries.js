7564ccc2efce721599164150a9f4213a34f80c91
UFOsmiling <zhangmeiru@cityuniversity.edu>
2025-06-02 20:37:49 -0700

    Submission for PE06

---
+++ PE06/PE06-CitiesWithCountriesCurrencyStackNav/src/Countries/Countries.js
@@ -0,0 +1,130 @@
+// import React from "react";
+// import {
+//   View,
+//   Text,
+//   StyleSheet,
+//   TouchableWithoutFeedback,
+//   ScrollView,
+// } from "react-native";
+// import CenterMessage from "../components/CenterMessage";
+// import { colors } from "../theme";
+
+// export default class Countries extends React.Component {
+//   static navigationOptions = {
+//     title: "Countries",
+//     headerTitleStyle: {
+//       color: "white",
+//       fontSize: 20,
+//       fontWeight: "400",
+//     },
+//   };
+
+//   navigate = (item) => {
+//     this.props.navigation.navigate("Country", { country: item });
+//   };
+
+//   render() {
+//     const { countries } = this.props;
+//     return (
+//       <ScrollView contentContainerStyle={[!countries.length && { flex: 1 }]}>
+//         <View
+//           style={[!countries.length && { justifyContent: "center", flex: 1 }]}
+//         >
+//           {!countries.length && <CenterMessage message="No saved Countries!" />}
+//           {countries.map((item, index) => (
+//             <TouchableWithoutFeedback
+//               onPress={() => this.navigate(item)}
+//               key={index}
+//             >
+//               <View style={styles.cityContainer}>
+//                 <Text style={styles.city}>{item.country}</Text>
+//                 <Text style={styles.country}>{item.currency}</Text>
+//               </View>
+//             </TouchableWithoutFeedback>
+//           ))}
+//         </View>
+//       </ScrollView>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   cityContainer: {
+//     padding: 10,
+//     borderBottomWidth: 2,
+//     borderBottomColor: colors.primary,
+//   },
+//   city: {
+//     fontSize: 20,
+//   },
+//   country: {
+//     color: "rgba(0, 0, 0, .5)",
+//   },
+// });
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  ScrollView,
+} from "react-native";
+import CenterMessage from "../components/CenterMessage";
+import { colors } from "../theme";
+
+export default class Countries extends React.Component {
+  static navigationOptions = {
+    title: "Countries",
+    headerTitleStyle: {
+      color: "white",
+      fontSize: 20,
+      fontWeight: "400",
+    },
+  };
+
+  navigate = (item) => {
+    this.props.navigation.navigate("Country", {
+      country: item,
+      countries: this.props.countries,
+      addCurrency: this.props.addCurrency,
+    });
+  };
+
+  render() {
+    const { countries } = this.props;
+    return (
+      <ScrollView contentContainerStyle={[!countries.length && { flex: 1 }]}>
+        <View
+          style={[!countries.length && { justifyContent: "center", flex: 1 }]}
+        >
+          {!countries.length && <CenterMessage message="No saved countries!" />}
+          {countries.map((item, index) => (
+            <TouchableWithoutFeedback
+              onPress={() => this.navigate(item)}
+              key={index}
+            >
+              <View style={styles.cityContainer}>
+                <Text style={styles.city}>{item.country}</Text>
+                <Text style={styles.country}>{item.currency}</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          ))}
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  cityContainer: {
+    padding: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: colors.primary,
+  },
+  city: {
+    fontSize: 20,
+  },
+  country: {
+    color: "rgba(0, 0, 0, .5)",
+  },
+});
